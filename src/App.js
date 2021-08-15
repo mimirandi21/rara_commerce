472e48f896a9a43a4cbe141763318f2af9395a24
mimirandi21 <mimirandi21@gmail.com>
2021-08-15 14:02:50 -0500

    added request to commerce.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
-
-import { Products, NavBar } from './components'
+import React, { useState, useEffect } from "react";
+import { commerce } from "./lib/commerce";
+import { Products, NavBar } from "./components";
 //^^this is a cleaner way to import the lines below
 //import Products from "./components/products/Products";
 //import NavBar from './components/NavBar/NavBar'
 //for this abbreviation to work we must create an index.js file in our components directory
 //and export all of the components from that file
 const App = () => {
-  return (
-    <div>
-      <NavBar/>
-      <Products />
-    </div>
-  );
+	const [products, setProducts] = useState([]);
+
+	const fetchProducts = async () => {
+		const { data } = await commerce.products.list();
+
+		setProducts(data);
+	};
+
+	useEffect(() => {
+		fetchProducts();
+	}, []);
+
+	return (
+		<div>
+			<NavBar />
+			<Products products={products} />
+		</div>
+	);
 };
 
 export default App;
--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,2 +1,6 @@
-import Commerce from '@chec/commerce.js'
-export const commmerce= new Commerce()
+import Commerce from "@chec/commerce.js";
+
+export const commerce = new Commerce(
+	process.env.REACT_APP_CHEC_PUBLIC_KEY,
+	true
+);

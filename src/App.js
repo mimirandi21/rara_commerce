7535ae308be2b4f9f683bcc9137d06b562a47d33
mimirandi21 <mimirandi21@gmail.com>
2021-08-31 09:08:08 -0500

    add handleUpdateCartQty, update handleAddToCart

--- src/App.js
+++ src/App.js
@@ -8,46 +8,53 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 //for this abbreviation to work we must create an index.js file in our components directory
 //and export all of the components from that file
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState({});
+	const [products, setProducts] = useState([]);
+	const [cart, setCart] = useState({});
 
-  const fetchProducts = async () => {
-    const { data } = await commerce.products.list();
+	const fetchProducts = async () => {
+		const { data } = await commerce.products.list();
 
-    setProducts(data);
-  };
+		setProducts(data);
+	};
 
-  const fetchCart = async () => {
-    const cart = await commerce.cart.retrieve();
-    setCart(cart);
-  };
+	const fetchCart = async () => {
+		const cart = await commerce.cart.retrieve();
+		setCart(cart);
+	};
 
-  const handleAddToCart = async (productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
-    setCart(item.cart);
-  };
-  useEffect(() => {
-    fetchProducts();
-    fetchCart();
-  }, []);
+	const handleAddToCart = async (productId, quantity) => {
+		const { cart } = await commerce.cart.add(productId, quantity);
+		setCart(cart);
+	};
 
-  console.log(products);
-  console.log(cart);
-  return (
-    <Router>
-      <div>
-        <NavBar totalItems={cart.total_items} />
-        <Switch>
-          <Route exact path="/">
-            <Products products={products} onAddToCart={handleAddToCart} />
-          </Route>
-          <Route exact path="/cart">
-            <Cart cart={cart} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	const handleUpdateCartQty = async (productId, quantity) => {
+		const { cart } = await commerce.cart.update(productId, { quantity });
+
+		setCart(cart);
+	};
+
+	useEffect(() => {
+		fetchProducts();
+		fetchCart();
+	}, []);
+
+	console.log(products);
+	console.log(cart);
+	return (
+		<Router>
+			<div>
+				<NavBar totalItems={cart.total_items} />
+				<Switch>
+					<Route exact path="/">
+						<Products products={products} onAddToCart={handleAddToCart} />
+					</Route>
+					<Route exact path="/cart">
+						<Cart cart={cart} />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
